e7b0be116fc42c7ac5136e3a0651d59a71c9693a
Rijenth <a_rijenth@laposte.net>
2023-11-13 20:58:57 +0100

    add authentication store

---
+++ src/state/authentication/authentication.ts
@@ -0,0 +1,27 @@
+import { createSlice } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit'
+
+export interface AuthenticationState {
+    token: string|null
+}
+
+export const initialState: AuthenticationState = {
+    token: null,
+}
+
+export const authenticationSlice = createSlice({
+    name: 'authentication',
+    initialState,
+    reducers: {
+        login: (state: AuthenticationState, action: PayloadAction<string>) => {
+            state.token = action.payload;
+        },
+        logout: (state: AuthenticationState) => {
+            state.token = null;
+        }
+    },
+})
+
+export const { login, logout } = authenticationSlice.actions
+
+export default authenticationSlice.reducer
