59b1611f7de270bc84d7a75a3aece6a0ac1ed5bf
Rijenth <a_rijenth@laposte.net>
2023-11-13 21:40:34 +0100

    add guidance

--- src/containers/dashboard/dashboard.tsx
+++ src/containers/dashboard/dashboard.tsx
@@ -10,20 +10,10 @@ export default function Dashboard() {
         <div>
             <h1>Dashboard</h1>
 
-            {token === null && (
-                <>
-                    <p className="text-black">Vous n'êtes pas connecté</p>
-                </>
-            )}
-
-            {token !== null && (
-                <>
-                    <p className="text-black">Vous êtes connecté</p>
-                    <p className="text-black">Token: {token}</p>
-                </>
-            )}
-
-
+            <>
+                <p className="text-black">Vous êtes connecté</p>
+                <p className="text-black">Token: {token}</p>
+            </>
 
             <button className="btn btn-primary mt-2">
                 <Link to="/" className='text-white'>Login</Link>
