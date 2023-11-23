4bb6043c1120e65bd0749111a6cf35a657c21ff1
Hassan Aidibe <101555873+HascoVice@users.noreply.github.com>
2023-11-23 14:32:24 +0100

    ajout component input

---
+++ src/components/TextInput/TextInput.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+interface TextInputProps {
+    label: string;
+    type: string;
+    placeholder: string;
+    value: string;
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    minLength?: number;
+    required?: boolean;
+}
+
+const TextInput: React.FC<TextInputProps> = ({
+    label,
+    type,
+    placeholder,
+    value,
+    onChange,
+    minLength,
+    required,
+}: TextInputProps) => {
+    return (
+        <div>
+            <label className="label">
+                <span className="label-text">{label}</span>
+            </label>
+            <input
+                type={type}
+                placeholder={placeholder}
+                className="input input-bordered w-full max-w-xs rounded-xl"
+                value={value}
+                onChange={onChange}
+                minLength={minLength}
+                required={required}
+            />
+        </div>
+    );
+};
+
+export default TextInput;
--- src/components/chatcontent/chatitem.tsx
+++ src/components/chatcontent/chatitem.tsx
@@ -2,28 +2,37 @@ import React from 'react';
 import Avatar from '../chatlist/avatar';
 
 interface ChatItemProps {
-    user: string;
     msg: string;
     image: string;
     animationDelay: number;
+    id: number;
+    content: string;
+    conversation_id?: number;
+    user_id?: number;
+    created_at?: number;
+    received_at?: null;
+    channel?: null;
 }
-
 const ChatItem: React.FC<ChatItemProps> = ({
-    user,
     msg,
-    image,
+    content,
     animationDelay,
+    image,
+    id,
+    received_at,
+    created_at,
 }: ChatItemProps) => {
     return (
         <div
             style={{ animationDelay: `0.${animationDelay}s` }}
-            className={`chat__item ${user ? user : ''}`}
+            className={`chat__item ${id ? id : ''}`}
         >
             <div className="chat__item__content">
                 <div className="chat__msg">{msg}</div>
+                <div className="chat__content">{content}</div>
                 <div className="chat__meta">
-                    <span>16 mins ago</span>
-                    <span>Seen 1.03PM</span>
+                    <span>{received_at}</span>
+                    <span>{created_at}</span>
                 </div>
             </div>
             <Avatar image={image} />
