6d2123794295a5ca9ecab84363e066b0acbf4bf1
Rijenth <a_rijenth@laposte.net>
2023-11-26 13:10:03 +0100

    remove UnAuthenticatedRoute

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,7 @@ import './App.css';
 import Dashboard from './containers/dashboard/dashboard';
 import { useSelector } from 'react-redux';
 import { RootState } from './store/store';
-
-const UnAuthenticatedRoute: React.FC = () => {
-    const token = useSelector((state: RootState) => state.authentication.token);
-
-    if (token) {
-        return <Navigate to="/dashboard" replace={true} />;
-    }
-
-    return <Outlet />;
-};
+import TestingComponent from './containers/test/TestingComponent';
 
 const AuthenticatedRoute: React.FC = () => {
     const token = useSelector((state: RootState) => state.authentication.token);
@@ -31,15 +22,15 @@ function App(): JSX.Element {
     return (
         <>
             <Routes>
-                <Route element={<UnAuthenticatedRoute />}>
-                    <Route path="/" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/conversation" element={<Conversation />} />
-                </Route>
+                <Route path="/" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/conversation" element={<Conversation />} />
 
                 <Route element={<AuthenticatedRoute />}>
                     <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/test" element={<TestingComponent />} />
                 </Route>
+
                 <Route path="*" element={<h1>404</h1>} />
             </Routes>
         </>
