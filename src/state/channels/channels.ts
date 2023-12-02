183682949fcf41af6da27dab0d3f789a04501e98
Rijenth <a_rijenth@laposte.net>
2023-12-02 11:27:31 +0100

    create conversations and channels store

---
+++ src/state/channels/channels.ts
@@ -0,0 +1,51 @@
+import { createSelector, createSlice } from '@reduxjs/toolkit';
+import { IChannel } from '../../interfaces/channel/IChannel';
+import { RootState } from '../../store/store';
+import { IMessage } from '../../interfaces/message/IMessage';
+
+const initialState: { data: IChannel[] } = {
+    data: [],
+};
+
+export const channelsSlice = createSlice({
+    name: 'channels',
+    initialState,
+    reducers: {
+        resetToDefault: () => initialState,
+        setChannels: (
+            state,
+            action: {
+                type: string;
+                payload: {
+                    channels: IChannel[];
+                };
+            }
+        ) => {
+            return {
+                ...state,
+                data: action.payload.channels,
+            };
+        },
+    },
+});
+
+const selectChannels = (state: RootState): IChannel[] => state.channels.data;
+
+type Selector<S> = (state: RootState) => S;
+
+export const selectMessagesByChannelId = (
+    channelId: number
+): Selector<IMessage[]> =>
+    createSelector([selectChannels], (channels: IChannel[]) => {
+        const channel = channels.find((c: IChannel) => c.id === channelId);
+
+        if (typeof channel === 'undefined') {
+            return [];
+        }
+
+        return channel.messages;
+    });
+
+export const { resetToDefault, setChannels } = channelsSlice.actions;
+
+export default channelsSlice.reducer;
