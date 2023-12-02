b8eda19b01662f79332d252f90e036e2de2bac8c
Rijenth <a_rijenth@laposte.net>
2023-12-02 11:26:50 +0100

    change interfaces ext

--- src/interfaces/message/IMessage.ts
+++ src/interfaces/message/IMessage.ts
@@ -1,9 +1,14 @@
 export interface IMessage {
     id: number;
     content: string;
-    channel_id: number | null;
+    channel: IMessageChannel | null;
     conversation_id: number | null;
     user_id: number;
     created_at: Date;
     received_at: Date | null;
+}
+
+interface IMessageChannel {
+    id: number;
+    name: string;
 }
