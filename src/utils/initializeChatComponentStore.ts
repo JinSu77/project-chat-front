88fb1197646361a29274f2fb62278afd9d45e2c3
Rijenth <a_rijenth@laposte.net>
2024-01-06 12:16:52 +0100

    fix: redirect to login if error 401 when init app data

--- src/utils/initializeChatComponentStore.ts
+++ src/utils/initializeChatComponentStore.ts
@@ -11,84 +11,89 @@ const initializeChatComponentStore = async (
     userId?: number
 ): Promise<void> => {
     if (typeof token === 'string' && typeof userId === 'number') {
-        const [
-            fetchChannelsResponse,
-            fetchContactsResponse,
-            fetchConversationsResponse,
-        ] = await Promise.all([
-            fetch('http://localhost:8000/channels', {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `Bearer ${token}`,
-                },
-            }),
-            fetch(`http://localhost:8000/users/${userId}/contacts`, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `Bearer ${token}`,
-                },
-            }),
-            fetch(`http://localhost:8000/users/${userId}/conversations`, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `Bearer ${token}`,
-                },
-            }),
-        ]);
+        try {
+            const [
+                fetchChannelsResponse,
+                fetchContactsResponse,
+                fetchConversationsResponse,
+            ] = await Promise.all([
+                fetch('http://localhost:8000/channels', {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${token}`,
+                    },
+                }),
+                fetch(`http://localhost:8000/users/${userId}/contacts`, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${token}`,
+                    },
+                }),
+                fetch(`http://localhost:8000/users/${userId}/conversations`, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${token}`,
+                    },
+                }),
+            ]);
 
-        if (
-            !fetchChannelsResponse.ok ||
-            !fetchConversationsResponse.ok ||
-            !fetchContactsResponse.ok
-        ) {
+            if (
+                !fetchChannelsResponse.ok ||
+                !fetchConversationsResponse.ok ||
+                !fetchContactsResponse.ok
+            ) {
+                handleLogout(dispatch, null, false);
+                return;
+            }
+
+            const [channelsJson, contactsJson, conversationsJson] =
+                await Promise.all([
+                    fetchChannelsResponse.json(),
+                    fetchContactsResponse.json(),
+                    fetchConversationsResponse.json(),
+                ]);
+
+            dispatch({
+                type: 'channels/setChannels',
+                payload: channelsJson.data,
+            });
+
+            dispatch({
+                type: 'contacts/setContacts',
+                payload: contactsJson.data,
+            });
+
+            dispatch({
+                type: 'conversations/setConversations',
+                payload: conversationsJson.data,
+            });
+
+            const topics = [
+                ...(channelsJson.data.channels || []).map(
+                    (channel: IChannel) => `/channels/${channel.id}`
+                ),
+                ...(contactsJson.data.contacts || []).map(
+                    (contacts: IUser) => `/contacts/${contacts.id}`
+                ),
+                ...(conversationsJson.data.conversations || []).map(
+                    (conversation: IConversation) =>
+                        `/conversations/${conversation.id}`
+                ),
+            ];
+
+            dispatch({
+                type: 'mercure/setTopics',
+                payload: {
+                    topics: topics,
+                },
+            });
+        } catch (e) {
             handleLogout(dispatch, null, false);
             return;
         }
-
-        const [channelsJson, contactsJson, conversationsJson] =
-            await Promise.all([
-                fetchChannelsResponse.json(),
-                fetchContactsResponse.json(),
-                fetchConversationsResponse.json(),
-            ]);
-
-        dispatch({
-            type: 'channels/setChannels',
-            payload: channelsJson.data,
-        });
-
-        dispatch({
-            type: 'contacts/setContacts',
-            payload: contactsJson.data,
-        });
-
-        dispatch({
-            type: 'conversations/setConversations',
-            payload: conversationsJson.data,
-        });
-
-        const topics = [
-            ...(channelsJson.data.channels || []).map(
-                (channel: IChannel) => `/channels/${channel.id}`
-            ),
-            ...(contactsJson.data.contacts || []).map(
-                (contacts: IUser) => `/contacts/${contacts.id}`
-            ),
-            ...(conversationsJson.data.conversations || []).map(
-                (conversation: IConversation) =>
-                    `/conversations/${conversation.id}`
-            ),
-        ];
-
-        dispatch({
-            type: 'mercure/setTopics',
-            payload: {
-                topics: topics,
-            },
-        });
     }
 };
 
